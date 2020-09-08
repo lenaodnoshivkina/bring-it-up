3faac6bad639187af5d17932c39f4a3db76b3344
Elena <elena.odnoshivkina@mail.ru>
2020-09-09 00:56:19 +0300

    Add modal hanson

--- dist/script.js
+++ dist/script.js
@@ -974,6 +974,8 @@ function () {
   _createClass(Slider, [{
     key: "showSlides",
     value: function showSlides(n) {
+      var _this = this;
+
       if (n > this.slides.length) {
         this.slideIndex = 1;
       }
@@ -981,6 +983,21 @@ function () {
       if (n < 1) {
         this.slideIndex = this.slides.length;
       }
+
+      try {
+        this.hanson.style.opacity = '0';
+
+        if (n === 3) {
+          this.hanson.classList.add('animated');
+          setTimeout(function () {
+            _this.hanson.style.opacity = '1';
+
+            _this.hanson.classList.add('slideInUp');
+          }, 3000);
+        } else {
+          this.hanson.classList.remove('slideInUp');
+        }
+      } catch (e) {}
 
       this.slides.forEach(function (slide) {
         slide.classList.add("animated");
@@ -996,25 +1013,29 @@ function () {
   }, {
     key: "render",
     value: function render() {
-      var _this = this;
+      var _this2 = this;
+
+      try {
+        this.hanson = document.querySelector('.hanson');
+      } catch (e) {}
 
       this.btns.forEach(function (item) {
         item.addEventListener('click', function () {
-          _this.plusSlides(1);
+          _this2.plusSlides(1);
 
-          _this.slides[_this.slideIndex - 1].classList.remove('slideInUp');
+          _this2.slides[_this2.slideIndex - 1].classList.remove('slideInUp');
 
-          _this.slides[_this.slideIndex - 1].classList.add('slideInDown');
+          _this2.slides[_this2.slideIndex - 1].classList.add('slideInDown');
         });
         item.parentNode.previousElementSibling.addEventListener('click', function (e) {
           e.preventDefault();
-          _this.slideIndex = 1;
+          _this2.slideIndex = 1;
 
-          _this.showSlides(_this.slideIndex);
+          _this2.showSlides(_this2.slideIndex);
 
-          _this.slides[_this.slideIndex - 1].classList.remove('slideInUp');
+          _this2.slides[_this2.slideIndex - 1].classList.remove('slideInUp');
 
-          _this.slides[_this.slideIndex - 1].classList.add('slideInDown');
+          _this2.slides[_this2.slideIndex - 1].classList.add('slideInDown');
         });
       });
       this.showSlides(this.slideIndex); //первичная инициализация слайдера
